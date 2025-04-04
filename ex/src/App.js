51eb501e569fbaca79b54e431f716d68261072e9
Imam ariadi <gamerapin139@gmail.com>
2025-04-04 15:40:34 +0700

    1 component done

---
+++ ex/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import TemperatureGauge from './components/Geuge'; // Tambahkan impor komponen
+
+function App() {
+  return (
+    <div className="App">
+      {/* Tambahkan komponen TemperatureGauge di sini */}
+      <TemperatureGauge />
+    </div>
+  );
+}
+
+export default App;
